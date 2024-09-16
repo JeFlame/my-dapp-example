655020d6ad28aed250504c2a19075cc062c918e2
Hoang Vu <hoangvu130301@gmail.com>
2024-09-16 22:30:51 +0700

    feat: init ltv30 contract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,90 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.24;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
-
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
-
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
+contract LTV30 is
+	ERC721,
+	ERC721Enumerable,
+	ERC721URIStorage,
+	ERC721Burnable,
+	AccessControl
+{
+	struct Expression {
+		address owner;
+		uint256 tokenId;
+		string textIpfsCid;
+		string imageIpfsCid;
 	}
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
+	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+	uint256 public tokenCounter;
+
+	mapping(address => Expression) expressions;
+
+	constructor(address defaultAdmin, address minter) ERC721("LTV30", "LTV30") {
+		_grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+		_grantRole(MINTER_ROLE, minter);
 	}
 
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
-
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
-
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
+	function submit(
+		string calldata textIpfsCid,
+		string calldata imageIpfsCid
+	) external {
+		address sender = _msgSender();
+		if (expressions[sender].tokenId != 0) {
+			revert("already");
 		}
 
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+		uint256 tokenId = ++tokenCounter;
+		Expression memory expression = Expression({
+			owner: sender,
+			tokenId: tokenId,
+			textIpfsCid: textIpfsCid,
+			imageIpfsCid: imageIpfsCid
+		});
+
+		expressions[sender] = expression;
+
+		_safeMint(sender, tokenId);
+		_setTokenURI(tokenId, imageIpfsCid);
 	}
 
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
+	// The following functions are overrides required by Solidity.
+
+	function _update(
+		address to,
+		uint256 tokenId,
+		address auth
+	) internal override(ERC721, ERC721Enumerable) returns (address) {
+		return super._update(to, tokenId, auth);
 	}
 
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+	function _increaseBalance(
+		address account,
+		uint128 value
+	) internal override(ERC721, ERC721Enumerable) {
+		super._increaseBalance(account, value);
+	}
+
+	function tokenURI(
+		uint256 tokenId
+	) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+		return super.tokenURI(tokenId);
+	}
+
+	function supportsInterface(
+		bytes4 interfaceId
+	)
+		public
+		view
+		override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl)
+		returns (bool)
+	{
+		return super.supportsInterface(interfaceId);
+	}
 }
